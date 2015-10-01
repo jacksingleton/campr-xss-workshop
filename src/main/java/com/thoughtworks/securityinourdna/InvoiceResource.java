8d17d575df6dfe715a5e54aa1d429c9a1a63f700
Jack Singleton <git@jacksingleton.com>
2015-10-01 12:34:51 -0700

    add solution

--- src/main/java/com/thoughtworks/securityinourdna/InvoiceResource.java
+++ src/main/java/com/thoughtworks/securityinourdna/InvoiceResource.java
@@ -3,9 +3,8 @@ package com.thoughtworks.securityinourdna;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
-
-// TODO: Maybe use this after done writing features?
 import org.owasp.encoder.Encode;
 
 @RestController
@@ -25,6 +24,13 @@ public class InvoiceResource {
 
     @RequestMapping(value = "/invoices", method = RequestMethod.GET)
     public List<String> getAllInvoices() throws Exception {
-        return invoiceRepo.all();
+        List<String> rawInvoices = invoiceRepo.all();
+
+        List<String> safeInvoices = new ArrayList<>();
+        for (String rawInvoice : rawInvoices) {
+            safeInvoices.add(Encode.forHtml(rawInvoice));
+        }
+
+        return safeInvoices;
     }
 }
